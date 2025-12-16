6696f5bd4dbd133a5258e954ad8a2a7c886d2472
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2025-12-16 07:33:42 -0300

    end of section 2

---
+++ API/Data/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using API.Enttities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class AppDbContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<AppUser> Users { get; set; }
+}
